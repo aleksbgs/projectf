27bf6ae0d220ab57e401486d5abfe0942137a84b
Aleksandar Markovic <aleksbgs@yahoo.com>
2022-07-31 13:36:00 +0200

    add create user

--- faceit/server/main.go
+++ faceit/server/main.go
@@ -1,1 +1,44 @@
-package server
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/aleksbgs/projectf/faceit/proto"
+	"google.golang.org/grpc"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("faceit").Collection("users")
+
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+
+	log.Printf("Listening at %s\n", addr)
+
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &Server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
---
+++ faceit/server/server.go
@@ -0,0 +1,7 @@
+package main
+
+import pb "github.com/aleksbgs/projectf/faceit/proto"
+
+type Server struct {
+	pb.UserServiceServer
+}
